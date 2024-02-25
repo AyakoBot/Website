2b914356fa8bba103708601efc9f2a64b6123e4b
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-02-26 00:03:41 +0100

    basics finished

--- src/lib/components/page/Footer.svelte
+++ src/lib/components/page/Footer.svelte
@@ -2,33 +2,48 @@
 	import ColorFadeText from '$lib/components/generic/colorFadeText.svelte';
 </script>
 
-<footer class="bg-neutral-900/75 w-full box-shadow-main">
+<footer class="bg-neutral-900/75 w-full box-shadow-main flex md:mt-0 mt-4">
 	<ul class="flex flex-col justify-around items-center w-full">
-		<li class="flex flex-row items-center justify-evenly w-full">
-			<ul id="left">
-				<li class="font-size-6xl"><ColorFadeText text="Ayako" /></li>
-				<li>ayakobot.com</li>
+		<li class="flex flex-row items-center justify-evenly w-full flex-wrap">
+			<ul id="left" class="flex flex-row items-center justify-evenly">
+				<li class="pr-4">
+					<a href="/"
+						><img
+							src="https://cdn.ayakobot.com/website_assets/Icon.png"
+							alt="Ayako's Icon"
+       class="md:w-128px md:h-128px w-64px h-64px"
+							width="128"
+							height="128"
+						/></a
+					>
+				</li>
+				<li>
+					<ul class="flex flex-col justify-around">
+						<li class="md:text-6xl text-4xl font-bold"><ColorFadeText text="Ayako" /></li>
+						<li class="md:text-4xl text-2xl">ayakobot.com</li>
+					</ul>
+				</li>
 			</ul>
 
 			<ul id="right" class="flex flex-row justify-around items-center my-6">
 				<li>
 					<ul class="flex flex-col justify-around items-start px-4">
-						<li><a class="btn-quiet text-5" href="https://support.ayakobot.com">Support</a></li>
-						<li><a class="btn-quiet text-5 color-yellow" href="/donate">👑Premium👑</a></li>
+						<li><a class="btn-quiet text-5 font-bold" href="https://support.ayakobot.com">Support</a></li>
+						<li><a class="btn-quiet text-5 font-bold color-yellow" href="/donate">👑Premium👑</a></li>
 						<li>
-							<a class="text-5" href="https://top.gg/bot/650691698409734151/vote"
+							<a class="text-5 font-bold" href="https://top.gg/bot/650691698409734151/vote"
 								><ColorFadeText text="Vote" /></a
 							>
 						</li>
-						<li><a class="text-5" href="/donate"><ColorFadeText text="Donate" /></a></li>
+						<li><a class="text-5 font-bold" href="/donate"><ColorFadeText text="Donate" /></a></li>
 					</ul>
 				</li>
 				<li>
 					<ul class="flex flex-col justify-around items-start px-4">
-						<li><a class="btn-quiet text-5" href="https://support.ayakobot.com">Animekos</a></li>
-						<li><a class="btn-quiet text-5" href="/credits">Creators</a></li>
-						<li><a class="btn-quiet text-5" href="/terms">Terms</a></li>
-						<li><a class="btn-quiet text-5" href="/privacy">Privacy</a></li>
+						<li><a class="btn-quiet text-5 font-bold" href="https://support.ayakobot.com">Animekos</a></li>
+						<li><a class="btn-quiet text-5 font-bold" href="/credits">Creators</a></li>
+						<li><a class="btn-quiet text-5 font-bold" href="/terms">Terms</a></li>
+						<li><a class="btn-quiet text-5 font-bold" href="/privacy">Privacy</a></li>
 					</ul>
 				</li>
 			</ul>
--- src/lib/components/page/NavBar.svelte
+++ src/lib/components/page/NavBar.svelte
@@ -1,8 +1,8 @@
 <nav
-	class="absolute top-0 w-full p-2 bg-neutral-900/75 flex flex-row justify-around items-center box-shadow-main"
+	class="absolute w-full p-2 bg-neutral-900/75 flex flex-col justify-between items-center box-shadow-main md:flex-row"
 >
-	<ul id="left" class="flex flex-row justify-start items-center w-80">
-		<li>
+	<ul id="left" class="flex flex-row items-center">
+		<li class="pr-2">
 			<a href="/">
 				<img
 					src="https://cdn.ayakobot.com/website_assets/NavBarIcon.png"
@@ -12,22 +12,27 @@
 				/></a
 			>
 		</li>
-		<li class="flex flex-row justify-between items-end w-40">
-			<p class="font-bold font-size-9 color-[#b0ff00]">Ayako</p>
-			<p class="break-keep whitespace-nowrap pb-1.5 color-[#fe3521] font-bold pl-2 font-size-5">
+		<li class="flex flex-row justify-between items-end w-40%">
+			<p class="font-bold lg:text-9 text-8 color-[#b0ff00]">Ayako</p>
+			<p class="break-keep whitespace-nowrap pb-1.5 color-[#fe3521] font-bold pl-2 lg:text-5 text-4">
 				Discord Bot
 			</p>
 		</li>
 	</ul>
 
-	<ul id="right" class="flex flex-row w-130 justify-evenly items-center font-size-5">
+	<ul
+		id="right"
+		class="flex flex-row w-full md:w-3/5 lg:w-1/2 justify-between md:justify-evenly items-center lg:py-0 py-3"
+	>
 		<li>
-			<a class="btn-medium" href="/appeals">Appeals</a>
+			<a class="btn-medium text-4 lg:text-5 font-bold" href="/appeals">Appeals</a>
 		</li>
 		<li>
-			<a class="btn-quiet" href="https://invite.ayakobot.com" target="_blank">Invite</a>
+			<a class="btn-quiet text-4 lg:text-5 font-bold" href="https://invite.ayakobot.com" target="_blank"
+				>Invite</a
+			>
 		</li>
-		<li><a class="btn-quiet" href="https://support.ayakobot.com">Support</a></li>
-		<li><a class="color-yellow btn-quiet" href="/donate">👑Premium👑</a></li>
+		<li><a class="btn-quiet text-4 lg:text-5 font-bold" href="https://support.ayakobot.com">Support</a></li>
+		<li><a class="color-yellow btn-quiet text-4 lg:text-5 font-bold" href="/donate">👑Premium👑</a></li>
 	</ul>
 </nav>
