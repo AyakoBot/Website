ae917d69da866d8fef722cedab44b51c099c184f
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-02-28 22:04:03 +0100

    Blob added

--- src/lib/components/generic/Blob.svelte
+++ src/lib/components/generic/Blob.svelte
@@ -1,42 +1,100 @@
 <script lang="ts">
-	import { spring } from 'svelte/motion';
+	import { onMount } from 'svelte';
 
-	const coords = spring(
-		{ x: 50, y: 50 },
-		{
-			stiffness: 0.1,
-			damping: 1,
-		},
-	);
+	let blobSlow: HTMLDivElement;
+	let blobFast: HTMLDivElement;
 
-	const size = spring(10);
+	const coords = { x: 0, y: 0 };
 
-	const classes =
-		'bg-white h-52 fixed aspect-square bg-gradient-to-r from-[#b0ff00] to-[#fe3521] blur-[150px] opacity-0 z-[-9998] of-hidden';
+	let lastUpdate = Date.now();
 
-	import { writable } from 'svelte';
+	onMount(() => {
+		[blobSlow, blobFast].forEach((b) => {
+			b?.animate([{ opacity: `0%` }, { opacity: `50%` }], {
+				duration: 3000,
+				delay: 2000,
+				fill: 'forwards',
+			});
+		});
+	});
 
-	const mouseX = writable(0);
-	const mouseY = writable(0);
+	$: {
+		const x = coords.x;
+		const y = coords.y;
 
-	const handleMouseMove = (event) => {
-		mouseX.set(event.clientX);
-		mouseY.set(event.clientY);
-	};
+		console.log('update');
+
+		[blobSlow, blobFast].forEach((b) => {
+			b?.animate(
+				{
+					left: `${x - window.scrollX}px`,
+					top: `${y - window.scrollY}px`,
+				},
+				{ duration: 3000, fill: 'forwards' },
+			);
+		});
+	}
 </script>
 
-<svelte:window on:mousemove={handleMouseMove} />
-
-<svg
+<svelte:window
 	on:mousemove={(e) => {
-		coords.set({ x: e.clientX, y: e.clientY });
+		if (lastUpdate + 10 < Date.now()) {
+			lastUpdate = Date.now();
+			coords.x = e.clientX;
+			coords.y = e.clientY;
+		}
 	}}
-	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
-	role="presentation"
-	class="h-full w-full overflow-hidden content-none"
->
-	<circle cx={$coords.x} cy={$coords.y} r={$size} style={classes} />
-</svg>
+/>
 
-<div></div>
+<div class="h-full w-full overflow-hidden content-none fixed">
+	<div
+		id="rotate"
+		class="bg-white h-52 absolute aspect-square rounded-full bg-gradient-to-r from-[#b0ff00] to-[#fe3521] z--9998 of-hidden ml--100px mt--100px blur-150 op-0"
+		bind:this={blobSlow}
+	/>
+	<div
+		id="rotateFaster"
+		class="bg-white h-52 absolute aspect-square rounded-full bg-gradient-to-r from-[#b0ff00] to-[#fe3521] z--9998 of-hidden ml--100px mt--100px blur-150 op-0"
+		bind:this={blobFast}
+	/>
+</div>
+
+<style scoped>
+	#rotate {
+		animation: spin 10s infinite;
+	}
+
+	#rotateFaster {
+		animation: spinFaster 5s infinite;
+	}
+
+	@keyframes spin {
+		from {
+			transform: rotate(0deg);
+		}
+
+		50% {
+			scale: 1 1.5;
+		}
+
+		to {
+			transform: rotate(360deg);
+		}
+	}
+
+	@keyframes spinFaster {
+		from {
+			transform: rotate(0deg);
+			scale: 1 1.7;
+		}
+
+		50% {
+			scale: 1.7 1;
+		}
+
+		to {
+			transform: rotate(-360deg);
+			scale: 1 1.7;
+		}
+	}
+</style>
