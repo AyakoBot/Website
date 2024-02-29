a8798848652ee5887e512c8bd00e4de34fc8502f
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-03-01 00:14:51 +0100

    add db

---
+++ src/lib/server/database.ts
@@ -0,0 +1,15 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+prisma.$use(async (params, next) => {
+ try {
+  const result = await next(params);
+  return result;
+ } catch (error) {
+  if (error instanceof Prisma.PrismaClientKnownRequestError) return null;
+  throw error;
+ }
+});
+
+export default prisma;
