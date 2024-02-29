f0fcb6c81c8573305a8ec926435c9b39c4860721
Larsundso <Larsundso3@gmail.com>
2024-03-01 00:38:17 +0100

    rem unlighthouse

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 import { sveltekit } from '@sveltejs/kit/vite';
-import Unlighthouse from '@unlighthouse/vite';
+// import Unlighthouse from '@unlighthouse/vite';
 import { defineConfig } from 'vite';
 import UnoCSS from '@unocss/svelte-scoped/vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), Unlighthouse({}), UnoCSS({ injectReset: '' })],
+	plugins: [sveltekit(), UnoCSS({ injectReset: '' })], // Unlighthouse({}),
 	server: {
 		proxy: {
 			'/api': {
