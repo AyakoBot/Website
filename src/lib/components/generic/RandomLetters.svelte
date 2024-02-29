4a1eb02e8c66d5d681667faf8f73efb90bc957d8
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-03-01 00:13:47 +0100

    add

---
+++ src/lib/components/generic/RandomLetters.svelte
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+	import { onMount } from 'svelte';
+	import ColorFade from './ColorFadeText.svelte';
+
+	export let text: string;
+	export let isTitle: boolean = false;
+	export let animate: boolean = false;
+	let randomLetters: HTMLElement | ColorFade;
+
+	let title = String(text);
+	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,!=+~/\\*&^%$#@1234567890:;`';
+
+	onMount(() => {
+		setTimeout(() => {
+			randomLetters.animate([{ opacity: 0 }, { opacity: 1 }], {
+				duration: 500,
+				easing: 'ease-in-out',
+				fill: 'forwards',
+			});
+
+			let iterations = 0;
+			const interval = setInterval(() => {
+				title = title
+					.split('')
+					.map((l, i) => {
+						if (l === ' ') return l;
+
+						if (i < iterations) return text[i];
+
+						const letter = letters[Math.floor(Math.random() * letters.length)];
+						return text[i] === text[i].toUpperCase() ? letter : letter.toLowerCase();
+					})
+					.join('');
+
+				if (iterations >= title.length) clearInterval(interval);
+
+				iterations += 1 / (isTitle ? 10 : 1);
+			}, 30);
+		}, 500);
+	});
+</script>
+
+<hi bind:this={randomLetters} class="code opacity-0">
+	{#if animate}
+		<ColorFade>{title}</ColorFade>
+	{:else}
+		{title}
+	{/if}
+</hi>
