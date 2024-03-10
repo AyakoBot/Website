c5328f3e19d1996f937a8097a5592692e28ee8e7
Larsundso <Larsundso3@gmail.com>
2024-03-10 10:32:28 +0100

    CountUp comps

---
+++ src/lib/components/generic/ColorFadeAndCountUp.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let num: number;
+	let element: HTMLParagraphElement;
+
+	onMount(() => {
+		const randomStartPosition = Math.floor(Math.random() * 101);
+		element.animate(
+			[
+				{ backgroundPosition: `${randomStartPosition}% center` },
+				{ backgroundPosition: `${randomStartPosition - 200}% center` },
+			],
+			{ duration: 3000, easing: 'linear', iterations: Infinity },
+		);
+
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((e) => {
+				if (e.isIntersecting) animateCountUp(num);
+				else {
+					counters.forEach((c) => clearInterval(c));
+					counters.clear();
+				}
+			});
+		});
+
+		if (element) observer.observe(element);
+	});
+
+	const counters: Map<number, number> = new Map();
+	$: currentCount = 0;
+
+	const animateCountUp = (targetValue: number) => {
+		const frameDuration = 1000 / 60;
+		const animationDuration = 4000;
+		const totalFrames = Math.round(animationDuration / frameDuration);
+
+		let frame = 0;
+		counters.set(
+			targetValue,
+			setInterval(() => {
+				frame += 1;
+				const progress = easeOutQuad(frame / totalFrames);
+				const countNow = Math.round(targetValue * progress);
+
+				if (currentCount !== countNow) currentCount = countNow;
+				if (frame === totalFrames) clearInterval(counters.get(targetValue));
+			}),
+		);
+	};
+
+	const easeOutQuad = (t: number) => t * (2 - t);
+	const numberWithCommas = (x: number) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+</script>
+
+<p
+	class="bg-gradient-to-r via-[#fe3521] from-[#b0ff00] to-[#b0ff00] bg-200 bg-repeat-x w-full m-auto bg-clip-text text-fill-color"
+	bind:this={element}
+>
+	{numberWithCommas(currentCount)}
+</p>
---
+++ src/lib/components/generic/ColorFadeAndRandomLetters.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let text: string;
+	export let isTitle: boolean = false;
+	let element: HTMLElement;
+
+	let title = String(text);
+	const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ_.,!=+~/\\*&^%$#@1234567890:;`';
+
+	onMount(() => {
+		const randomStartPosition = Math.floor(Math.random() * 101);
+		element.animate(
+			[
+				{ backgroundPosition: `${randomStartPosition}% center` },
+				{ backgroundPosition: `${randomStartPosition - 200}% center` },
+			],
+			{ duration: 3000, easing: 'linear', iterations: Infinity },
+		);
+
+		setTimeout(() => {
+			element.animate([{ opacity: 0 }, { opacity: 1 }], {
+				duration: 500,
+				easing: 'ease-in-out',
+				fill: 'forwards',
+			});
+
+			let iterations = 0;
+			const interval = setInterval(() => {
+				title = title
+					.split('')
+					.map((l, i) => {
+						if (l === ' ') return l;
+
+						if (i < iterations) return text[i];
+
+						const letter = letters[Math.floor(Math.random() * letters.length)];
+						return text[i] === text[i].toUpperCase() ? letter : letter.toLowerCase();
+					})
+					.join('');
+
+				if (iterations >= title.length) clearInterval(interval);
+
+				iterations += 1 / (isTitle ? 10 : 1);
+			}, 30);
+		}, 500);
+	});
+</script>
+
+<p
+	class="bg-gradient-to-r via-[#fe3521] from-[#b0ff00] to-[#b0ff00] bg-200 bg-repeat-x w-full m-auto bg-clip-text text-fill-color code opacity-0"
+	bind:this={element}
+>
+	{text}
+</p>
