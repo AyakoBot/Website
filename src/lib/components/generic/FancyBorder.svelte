2dc315a55a77d7e3f93f7b599a931e7582370275
Larsundso <Larsundso3@gmail.com>
2024-04-03 00:54:26 +0200

    add fancy border

---
+++ src/lib/components/generic/FancyBorder.svelte
@@ -0,0 +1,14 @@
+<div class="w-full h-40 relative">
+ <div
+  class="absolute inset-x-20 top-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm"
+ />
+ <div
+  class="absolute inset-x-20 top-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[1px] w-3/4"
+ />
+ <div
+  class="absolute inset-x-60 top-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm"
+ />
+ <div
+  class="absolute inset-x-60 top-0 left-1/2 -translate-x-1/2 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[1px] w-1/4"
+ />
+</div>
