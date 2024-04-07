0b224345b373aea52cbd8cc6dd1429e98cce7e70
Larsundso <Larsundso3@gmail.com>
2024-04-07 16:09:58 +0200

    nolog

--- src/routes/appeals/+page.server.ts
+++ src/routes/appeals/+page.server.ts
@@ -3,8 +3,5 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = (event) => {
 	const userId = event.cookies.get('discord-id');
-
 	if (!userId) redirect(307, '/login');
-
-	console.log(userId);
 };
