b85a485c951e5ae46f2a737639d2faf6ab21926a
Larsundso <Larsundso3@gmail.com>
2024-04-07 16:08:02 +0200

    proper typing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,12 @@
+import type { Handle } from '@sveltejs/kit';
+
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({
-	event,
-	resolve,
-}: {
-	resolve: (
-		e: typeof event,
-		{ transformPageChunk }: { transformPageChunk: (chunk: { html: string }) => void },
-	) => Promise<void>;
-	event: unknown;
-}) {
+export const handle: Handle = async ({ event, resolve }) => {
+	let buffer = '';
+
 	const response = await resolve(event, {
-		transformPageChunk: ({ html }: { html: string }) =>
-			html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles'),
+		transformPageChunk: (event) =>
+			event.html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles'),
 	});
 	return response;
-}
+};
