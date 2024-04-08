1bfe563c2f362578b8fa9d9f78a32809d4c68d91
Larsundso <Larsundso3@gmail.com>
2024-04-09 00:49:38 +0200

    global auth check

---
+++ src/lib/scripts/util/user2Cookies.ts
@@ -0,0 +1,27 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import type { APIUser } from 'discord-api-types/v10';
+
+export default async (req: RequestEvent) => {
+	const token = req.cookies.get('discord-token');
+	if (!token) return 403;
+
+	const userRes = await fetch('https://discord.com/api/v10/users/@me', {
+		headers: { Authorization: `Bearer ${token}` },
+	});
+
+	if (!userRes.ok) return 400;
+	const user = (await userRes.json()) as APIUser;
+
+	const basicCookieOptions: Parameters<typeof req.cookies.set>[2] = {
+		expires: new Date(Date.now() + 86400000),
+		path: '/',
+		maxAge: 86400,
+		sameSite: true,
+		httpOnly: false,
+		secure: false,
+	};
+
+	req.cookies.set('discord-id', user.id, basicCookieOptions);
+	req.cookies.set('discord-username', user.username, basicCookieOptions);
+	if (user.avatar) req.cookies.set('discord-avatar', user.avatar, basicCookieOptions);
+};
