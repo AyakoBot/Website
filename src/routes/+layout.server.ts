1876063acef5ab99b2c53e759845e6b5e9897fa7
Larsundso <Larsundso3@gmail.com>
2024-05-10 22:42:10 +0200

    monorepo-ification

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,3 @@
-import { onMount } from 'svelte';
-import { DiscordSDK } from '@discord/embedded-app-sdk';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => ({
@@ -7,9 +5,4 @@ export const load: LayoutServerLoad = async (event) => ({
 	pfp: event.cookies.get('discord-avatar')
 		? decodeURIComponent(event.cookies.get('discord-avatar')!)
 		: undefined,
-});
-
-onMount(async () => {
-	const SDK = new DiscordSDK(import.meta.env.VITE_ActivityID, { disableConsoleLogOverride: false });
-	await SDK.ready().then(() => console.log('SDK Ready'));
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,30 @@
 // @ts-expect-error
 import { sveltekit } from '@sveltejs/kit/vite';
 // import Unlighthouse from '@unlighthouse/vite';
-import { defineConfig } from 'vite';
+import { PluginOption, defineConfig } from 'vite';
 import UnoCSS from '@unocss/svelte-scoped/vite';
+
+/** @type {import('vite').Plugin} */
+const viteServerConfig = (): PluginOption => ({
+	name: 'add-headers',
+	configureServer: (server) => {
+		server.middlewares.use((req, res, next) => {
+   if (!req.url.startsWith('https://dev.ayakobot.com/api')) return next();
+   
+   res.setHeader('Access-Control-Allow-Origin', '*');
+			res.setHeader('Access-Control-Allow-Methods', '*');
+			res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
+			res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
+			return next();
+		});
+	},
+});
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		UnoCSS({ injectReset: '' }),
 		// Unlighthouse({}),
+		viteServerConfig(),
 	],
 });
