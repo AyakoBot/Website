6e838dda11ca0bc2374d976766b4c08b78b51c2a
Larsundso <Larsundso3@gmail.com>
2024-05-11 15:15:29 +0200

    api cors issues fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,18 @@
-import { type Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 /** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
+	if (event.url.href.includes('/api/')) {
+		event.url = new URL(
+			event.url.href
+				.replace('/api/', '/')
+				.replace('https://dev.ayakobot.com', 'https://apiv2.ayakobot.com')
+				.replace('http://dev.ayakobot.com', 'https://apiv2.ayakobot.com'),
+		);
+
+		return redirect(301, event.url);
+	}
+
 	const response = await resolve(event, {
 		transformPageChunk: (event) =>
 			event.html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles'),
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
+import { redirect } from '@sveltejs/kit';
 import { sveltekit } from '@sveltejs/kit/vite';
 // import Unlighthouse from '@unlighthouse/vite';
-import { PluginOption, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import UnoCSS from '@unocss/svelte-scoped/vite';
-
-/** @type {import('vite').Plugin} */
-const viteServerConfig = (): PluginOption => ({
-	name: 'add-headers',
-	configureServer: (server) => {
-		server.middlewares.use((req, res, next) => {
-   if (!req.url.startsWith('https://dev.ayakobot.com/api')) return next();
-   
-   res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', '*');
-			res.setHeader('Cross-Origin-Opener-Policy', 'unsafe-none');
-			res.setHeader('Cross-Origin-Embedder-Policy', 'unsafe-none');
-			return next();
-		});
-	},
-});
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		UnoCSS({ injectReset: '' }),
 		// Unlighthouse({}),
-		viteServerConfig(),
 	],
 });
