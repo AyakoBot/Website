e14f284428fac0d7fd64f62426bc10b4ce929100
Larsundso <Larsundso3@gmail.com>
2024-05-14 15:20:25 +0200

    post action

--- src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.server.ts
+++ src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.server.ts
@@ -1,15 +1,39 @@
 import { PUBLIC_API } from '$env/static/public';
-import type { PageServerLoad } from './$types';
+import { redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
 import type { Returned as GETCredits } from '@ayako/server/src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+server.js';
 
 export const load: PageServerLoad = async (event) => {
-	const appeal = await event.fetch(
-		`${PUBLIC_API}/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/appeal`,
-		{
-			headers: { Authorization: `Bearer ${event.cookies.get('discord-token')}` },
-		},
-	).then((r) => r.json() as Promise<GETCredits>);
+	const appeal = await event
+		.fetch(
+			`${PUBLIC_API}/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/appeal`,
+			{ headers: { Authorization: `Bearer ${event.cookies.get('discord-token')}` } },
+		)
+		.then((r) => r.json() as Promise<GETCredits>);
 
 	return { appeal };
 };
+
+export const actions = {
+	appeal: async (event) => {
+		const formData = await event.request.formData();
+		const formJSON: { [key: string]: string } = {};
+		formData.forEach((val, k) => (formJSON[k] = val.toString()));
+
+		const res = await event.fetch(
+			`${PUBLIC_API}/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/appeal`,
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${event.cookies.get('discord-token')}`,
+				},
+				method: 'POST',
+				body: JSON.stringify(formJSON),
+			},
+		);
+
+		if (!res.ok) return await res.json().then((r) => r.message);
+		throw redirect(308, `/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/status`);
+	},
+} satisfies Actions;
--- src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.svelte
+++ src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.svelte
@@ -1,38 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import { PUBLIC_API } from '$env/static/public';
+	import { enhance } from '$app/forms';
 	import FancyBorder from '$lib/components/generic/FancyBorder.svelte';
 	import Punishment from '$lib/components/page/appeals/Punishment.svelte';
 	import Question from '$lib/components/page/appeals/Question.svelte';
-	import * as Form from '$lib/components/ui/form';
-	import Cookies from 'js-cookie';
-	import type { PageServerData } from './$types';
+	import type { PageServerData, ActionData } from './$types';
 
 	export let data: PageServerData;
 
-	const guildId = $page.params.guildId;
-	const punId = $page.params.punishmentId;
-	let form: HTMLFormElement;
-
 	let submitted: boolean;
 	$: submitted = false;
-
-	const post = async (e: SubmitEvent & { currentTarget: EventTarget & HTMLFormElement }) => {
-		e.preventDefault();
-
-		const formData = new FormData(form, e.submitter);
-		const formJSON: { [key: string]: string } = {};
-		formData.forEach((val, k) => (formJSON[k] = val.toString()));
-
-		fetch(`${PUBLIC_API}/guilds/${guildId}/appeals/${punId}/appeal`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${Cookies.get('discord-token')}`,
-			},
-			body: JSON.stringify(formJSON),
-		});
-	};
 </script>
 
 <ul class="flex flex-col justify-center items-center mb-20 gap-5 min-w-100vw lg:min-w-50vw">
@@ -41,7 +17,12 @@
 </ul>
 
 {#if !('message' in data)}
-	<form class="flex flex-col justify-center items-center gap-5" on:submit={post} bind:this={form}>
+	<form
+		class="flex flex-col justify-center items-center gap-5"
+		method="post"
+		action="?/appeal"
+		use:enhance
+	>
 		{#each data.appeal.questions as question, i (i)}
 			{#if i}
 				<FancyBorder />
@@ -51,7 +32,7 @@
 		{/each}
 
 		<FancyBorder />
-		<p class="w-full px-2">
+		<div class="w-full px-2">
 			The information you provide in this form will be used to process your appeal. By submitting this
 			form, you agree to our <a href="/terms" target="_blank" class="underline">Terms of Service</a>
 			and
@@ -59,19 +40,19 @@
 			<br />
 			Never submit any sensitive information in this form. Provided information will be shared with selected
 			members of the server you appeal to.
-		</p>
+		</div>
 
-		<Form.Button class="btn-loud" disabled={submitted}>
+		<button class="btn-loud" disabled={submitted}>
 			{#if submitted}
 				Loading...
 			{:else}
 				Submit
 			{/if}
-		</Form.Button>
+		</button>
 	</form>
 {:else}
 	<div class="flex flex-col justify-center items-center">
-		<p>No Punishment, enabled Appeal-Settings, or enabled Appeal-Questions found</p>
+		<span>No Punishment, enabled Appeal-Settings, or enabled Appeal-Questions found</span>
 		<FancyBorder />
 		<a class="btn-loud" href="/appeals">Back</a>
 	</div>
