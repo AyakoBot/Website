38d68bbf8e20354d847dedf9ede40d1414f51cff
Larsundso <Larsundso3@gmail.com>
2024-05-17 23:06:20 +0200

    dont request

--- src/routes/artwork/+page.svelte
+++ src/routes/artwork/+page.svelte
@@ -1,28 +1,24 @@
-<script lang="ts" crossorigin="anonymous">
-	import { PUBLIC_API } from '$env/static/public';
+<script lang="ts">
 	import FancyBorder from '$lib/components/generic/FancyBorder.svelte';
 	import Loading from '$lib/components/generic/Loading.svelte';
 	import NoResults from '$lib/components/generic/NoResults.svelte';
 	import SearchBar from '$lib/components/generic/SearchBar.svelte';
 	import Image from '$lib/components/page/artwork/Image.svelte';
 	import { ArtType } from '$lib/scripts/types.js';
-	import type { Returned as GETArt } from '@ayako/server/src/routes/artworks/+server.js';
 	import type { PageServerData } from './$types.js';
 
-	export let data: PageServerData;
+	let type: ArtType = ArtType.all;
+	let query: string;
+	$: query = '';
 
-	const getArt = async (query?: string, type?: ArtType) => {
-		if (!query && !type) return data.art;
+	const assignFilters = async (q?: string, t?: ArtType) => {
+		if (!q && !t) return data.art;
 
-		const urlSearchParams = new URLSearchParams('');
-		if (query) urlSearchParams.set('q', query);
-		if (type) urlSearchParams.set('type', type);
-
-		const res = await fetch(`${PUBLIC_API}/artworks?${urlSearchParams.toString()}`);
-		return res.json() as Promise<GETArt>;
+		type = t || ArtType.all;
+		query = q?.toLowerCase() || '';
 	};
 
-	$: art = getArt();
+	export let data: PageServerData;
 </script>
 
 <div class="flex flex-col justify-center items-center w-full mt-25">
@@ -34,34 +30,29 @@
 				{ key: 'full', value: 'Full Image' },
 				{ key: 'icon', value: 'Icon' },
 			]}
-			on:any={(e) => (art = getArt(e.detail.query, e.detail.option as ArtType))}
+			on:any={(e) => (assignFilters(e.detail.query, e.detail.option as ArtType))}
 		/>
 
 		<FancyBorder />
 	</div>
 
 	<div class="w-full h-full m-auto mt-5">
-		{#await art}
-			<div class="w-100vw flex flex-row justify-center items-center">
-				<Loading />
+		{#if data.art.length}
+			<div
+				class="column-count-3 md:column-count-4 xl:column-count-5 column-width-80 w-full h-full gap-0"
+			>
+				{#each data.art.filter((a) => (type !== ArtType.all ? a.art.type === type : true) && (a.art.description ? a.art.description
+									.toLowerCase()
+									.includes(query) : true) && a.user.username
+							.toLowerCase()
+							.includes(query)) as artwork, i (i)}
+					<Image {artwork} />
+				{/each}
 			</div>
-		{:then art}
-			{#if art.length}
-				<div
-					class="column-count-3 md:column-count-4 xl:column-count-5 column-width-80 w-full h-full gap-0"
-				>
-					{#each art as artwork, i (i)}
-						<Image {artwork} />
-					{/each}
-				</div>
-			{:else}
-				<div class="w-100vw">
-					<NoResults />
-				</div>
-			{/if}
-		{:catch error}
-			{console.error(error)}
-			<span>Error</span>
-		{/await}
+		{:else}
+			<div class="w-100vw">
+				<NoResults />
+			</div>
+		{/if}
 	</div>
 </div>
