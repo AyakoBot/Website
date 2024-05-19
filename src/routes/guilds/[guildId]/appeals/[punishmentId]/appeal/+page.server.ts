4f0d651a9a4831d969be4ba3e2d8b8d3027c3779
Larsundso <Larsundso3@gmail.com>
2024-05-19 18:12:50 +0200

    redirect if already appealed

--- src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.server.ts
+++ src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+page.server.ts
@@ -2,7 +2,7 @@ import { PUBLIC_API } from '$env/static/public';
 import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
-import type { Returned as GETCredits } from '@ayako/server/src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+server.js';
+import type { Returned as GETAppeals } from '@ayako/server/src/routes/guilds/[guildId]/appeals/[punishmentId]/appeal/+server.js';
 
 export const load: PageServerLoad = async (event) => {
 	const appeal = await event
@@ -10,7 +10,14 @@ export const load: PageServerLoad = async (event) => {
 			`${PUBLIC_API}/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/appeal`,
 			{ headers: { Authorization: `Bearer ${event.cookies.get('discord-token')}` } },
 		)
-		.then((r) => r.json() as Promise<GETCredits>);
+		.then((r) => r.json() as Promise<GETAppeals>);
+
+	if (appeal.alreadyAppealed) {
+		throw redirect(
+			307,
+			`/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/status`,
+		);
+	}
 
 	return { appeal };
 };
@@ -34,6 +41,9 @@ export const actions = {
 		);
 
 		if (!res.ok) return await res.json().then((r) => r.message);
-		throw redirect(308, `/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/status`);
+		throw redirect(
+			308,
+			`/guilds/${event.params.guildId}/appeals/${event.params.punishmentId}/status`,
+		);
 	},
 } satisfies Actions;
