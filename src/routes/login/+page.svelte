80b92121f5d79a85441b286604901404c83cc670
Larsundso <Larsundso3@gmail.com>
2024-05-25 12:52:28 +0200

    not the correct place for this

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -36,12 +36,12 @@
 	</div>
 {:else}
 	<div class="flex flex-col justify-center items-center">
-		<Switch bind:checked title="Join Support Server" name="Join Support Server"/>
+		<Switch bind:checked title="Join Support Server" name="Join Support Server" />
 
 		<a
 			href="https://discord.com/oauth2/authorize?client_id={PUBLIC_ID}&response_type=code&redirect_uri={encodeURIComponent(
 				`https://ayakobot.com/login`,
-			)}&scope=identify+guilds{checked ? '+guilds.join' : ''}+applications.commands.permissions.update"
+			)}&scope=identify+guilds{checked ? '+guilds.join' : ''}"
 			class="text-xl btn-loud"
 		>
 			Log-In with Discord
