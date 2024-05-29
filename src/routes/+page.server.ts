c38e494b4ab48c8b4e49eec233124e7e3d29dc0b
Larsundso <Larsundso3@gmail.com>
2024-05-29 20:31:35 +0200

    handle errors

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,4 +1,6 @@
+import { DEBUG_ID, DEBUG_TOKEN } from '$env/static/private';
 import { PUBLIC_API } from '$env/static/public';
+
 import type { PageServerLoad } from './$types.js';
 
 import type { Returned as GETFeatures } from '@ayako/server/src/routes/features/+server';
@@ -6,16 +8,47 @@ import type { Returned as GETGuilds } from '@ayako/server/src/routes/guilds/+ser
 import type { Returned as GETReviews } from '@ayako/server/src/routes/reviews/+server.js';
 import type { Returned as GETStats } from '@ayako/server/src/routes/stats/+server.js';
 
+const handleError = async (
+	fetch: Parameters<PageServerLoad>[0]['fetch'],
+	r: Response,
+	fallback: any,
+) => {
+	fetch(`https://discord.com/api/webhooks/${DEBUG_ID}/${DEBUG_TOKEN}`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			content: `Request to \`${r.url}\` failed with Status Code \`${r.status}\ - \`${r.statusText}\``,
+			embeds: [
+				{
+					color: 0xff0000,
+					author: { name: 'Error', icon_url: 'https://cdn.ayakobot.com/Ayako_Assets/Warning.png' },
+					description: `\`\`\`${await r.text()}\`\`\``,
+				},
+			],
+		}),
+	});
+
+	return fallback;
+};
+
 export const load: PageServerLoad = async (event) => {
 	const [reviews, stats, features, guilds] = await Promise.all([
-		event.fetch(`${PUBLIC_API}/reviews`).then((r) => (r.ok ? (r.json() as Promise<GETReviews>) : [])),
+		event
+			.fetch(`${PUBLIC_API}/reviews`)
+			.then((r) => (r.ok ? (r.json() as Promise<GETReviews>) : handleError(event.fetch, r, []))),
 		event
 			.fetch(`${PUBLIC_API}/stats`)
-			.then((r) => (r.ok ? (r.json() as Promise<GETStats>) : { guildCount: 0, userCount: 0 })),
+			.then((r) =>
+				r.ok
+					? (r.json() as Promise<GETStats>)
+					: handleError(event.fetch, r, { guildCount: 0, userCount: 0 }),
+			),
 		event
 			.fetch(`${PUBLIC_API}/features?blocks=3`)
-			.then((r) => (r.ok ? (r.json() as Promise<GETFeatures>) : [])),
-		event.fetch(`${PUBLIC_API}/guilds`).then((r) => (r.ok ? (r.json() as Promise<GETGuilds>) : [])),
+			.then((r) => (r.ok ? (r.json() as Promise<GETFeatures>) : handleError(event.fetch, r, []))),
+		event
+			.fetch(`${PUBLIC_API}/guilds`)
+			.then((r) => (r.ok ? (r.json() as Promise<GETGuilds>) : handleError(event.fetch, r, []))),
 	]);
 
 	return {
