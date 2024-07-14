88c0796e7959f6b4f4d28094a187ea0da2c49a63
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2024-07-14 17:07:44 +0200

    Update +server.ts

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,17 +1,45 @@
 export const GET = () =>
 	new Response(
-		`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
-			<!-- <url> elements go here -->
-		</urlset>`.trim(),
+		`<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+  <loc>https://ayakobot.com/</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://ayakobot.com/donate</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://ayakobot.com/artwork</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://ayakobot.com/credits</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://ayakobot.com/terms</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://ayakobot.com/privacy</loc>
+  <lastmod>2024-07-14T15:02:22+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+
+
+</urlset>`.trim(),
 		{
 			headers: {
 				'Content-Type': 'application/xml',
