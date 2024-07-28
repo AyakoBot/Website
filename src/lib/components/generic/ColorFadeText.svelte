ad41bc8b4aa334ed3fccb5239c3d2365f7c70a84
Larsundso <Larsundso3@gmail.com>
2024-07-28 14:54:49 +0200

    no js animations

--- src/lib/components/generic/ColorFadeText.svelte
+++ src/lib/components/generic/ColorFadeText.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
- export let text: string;
-	let fade: HTMLElement;
-
-	onMount(() => {
-		const randomStartPosition = Math.floor(Math.random() * 101);
-		fade?.animate(
-			[
-				{ backgroundPosition: `${randomStartPosition}% center` },
-				{ backgroundPosition: `${randomStartPosition - 200}% center` },
-			],
-			{ duration: 3000, easing: 'linear', iterations: Infinity },
-		);
-	});
+	export let text: string;
+	const randomStartPosition = Math.floor(Math.random() * 101);
 </script>
 
 <div
-	class="bg-gradient-to-r via-[#fe3521] from-[#b0ff00] to-[#b0ff00] bg-[length:200%] bg-repeat-x w-full m-auto bg-clip-text text-fill-color font-bold"
-	bind:this={fade}
+	class="bg-gradient-to-r via-[#fe3521] from-[#b0ff00] to-[#b0ff00] bg-[length:200%] bg-repeat-x w-full m-auto bg-clip-text text-fill-color font-bold animate-[fade_3s_linear_infinite]"
+	style="--start-pos: {randomStartPosition}%"
 >
 	{text}
 </div>
+
+<style scoped>
+	@keyframes fade {
+		from {
+			background-position: var(--start-pos) center;
+		}
+		to {
+			background-position: calc(var(--start-pos) - 200%) center;
+		}
+	}
+</style>
