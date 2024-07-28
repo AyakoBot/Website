943ca38783f38cbda58061991de883eb926c1537
Larsundso <Larsundso3@gmail.com>
2024-07-28 14:44:18 +0200

    no js animations

--- src/lib/components/page/home/Flowers.svelte
+++ src/lib/components/page/home/Flowers.svelte
@@ -1,229 +1,58 @@
 <script setup lang="ts">
-	import { onMount, onDestroy } from 'svelte';
 	import ColorFadeText from '$lib/components/generic/ColorFadeText.svelte';
-
-	const animationOptions = {
-		duration: 2000,
-		delay: 500,
-		fill: 'forwards',
-		easing: 'cubic-bezier(0.03, 0.82, 0.13, 1.04)',
-	} as KeyframeAnimationOptions;
-
-	let flower1: HTMLElement | null;
-	let flower2: HTMLElement | null;
-	let flower3: HTMLElement | null;
-	let flower4: HTMLElement | null;
-	let flower5: HTMLElement | null;
-	let blendIn: HTMLElement | null;
-
-	const coords = {
-		sm: {
-			flower2: {
-				left: 'calc(50% - 33vmin)',
-				top: '17vmin',
-			},
-			flower3: {
-				left: 'calc(50% + 20vmin)',
-				top: '18vmin',
-			},
-			flower4: {
-				left: 'calc(50% + 10vmin)',
-				top: '12vmin',
-			},
-			flower5: {
-				left: 'calc(50% - 17vmin)',
-				top: '13vmin',
-			},
-		},
-
-		md: {
-			flower2: {
-				left: 'calc(50% - 33vmin)',
-				top: '15vmin',
-			},
-			flower3: {
-				left: 'calc(50% + 20vmin)',
-				top: '15vmin',
-			},
-			flower4: {
-				left: 'calc(50% + 11vmin)',
-				top: '10vmin',
-			},
-			flower5: {
-				left: 'calc(50% - 18vmin)',
-				top: '10vmin',
-			},
-		},
-
-		lg: {
-			flower2: {
-				left: 'calc(50% - 32vmin)',
-				top: '15vmin',
-			},
-			flower3: {
-				left: 'calc(50% + 21vmin)',
-				top: '15vmin',
-			},
-			flower4: {
-				left: 'calc(50% - 17vmin)',
-				top: '10vmin',
-			},
-			flower5: {
-				left: 'calc(50% + 14vmin)',
-				top: '10vmin',
-			},
-		},
-	};
-
-	let innerWidth: number = 0;
-
-	onMount(() => {
-		const sm = innerWidth < 768;
-		const md = innerWidth < 1024 && innerWidth >= 768;
-		const applyingCoords = coords[md ? 'md' : sm ? 'sm' : 'lg'];
-
-		blendIn?.animate([{ opacity: '0' }, { opacity: '1' }], animationOptions);
-
-		flower1?.animate(
-			[
-				{ top: '17vmin', opacity: '0' },
-				{ top: '0vmin', opacity: '1' },
-			],
-			animationOptions,
-		);
-
-		flower2?.animate(
-			[
-				{
-					left: 'calc(50% - 7vmin)',
-					top: '15vmin',
-					opacity: '0',
-					rotate: '-45deg',
-				},
-				{ ...applyingCoords.flower2, opacity: '1', rotate: '-80deg' },
-			],
-			animationOptions,
-		);
-
-		flower3?.animate(
-			[
-				{
-					left: 'calc(50% - 6vmin)',
-					top: '15vmin',
-					opacity: '0',
-					rotate: '12deg',
-				},
-				{ ...applyingCoords.flower3, opacity: '1', rotate: '50deg' },
-			],
-			animationOptions,
-		);
-
-		flower4?.animate(
-			[
-				{
-					left: 'calc(50% - 3vmin)',
-					top: '15vmin',
-					opacity: '0',
-					rotate: '-10deg',
-				},
-				{ ...applyingCoords.flower4, opacity: '1', rotate: '-35deg' },
-			],
-			animationOptions,
-		);
-
-		flower5?.animate(
-			[
-				{
-					left: 'calc(50% - 3vmin)',
-					top: '15vmin',
-					opacity: '0',
-					rotate: '80deg',
-				},
-				{ ...applyingCoords.flower5, opacity: '1', rotate: '100deg' },
-			],
-			animationOptions,
-		);
-	});
-
-	onDestroy(() => deleteElements());
-
-	const deleteElements = () => {
-		flower1?.remove();
-		flower2?.remove();
-		flower3?.remove();
-		flower4?.remove();
-		flower5?.remove();
-	};
 
 	const src = '/images/WhiteFlower.webp';
 	const alt = 'Flower';
 </script>
 
-<svelte:window bind:innerWidth />
-
 <div
-	class="flex justify-center items-center relative m-auto w-90vw h-22dvh md:h-30dvh mt-5% of-hidden of-y-hidden z-1"
+	class="flex justify-center items-center relative m-auto w-90vw h-30dvh mt-5% of-hidden of-y-hidden z-1"
+	style="--move: cubic-bezier(0.03, 0.82, 0.13, 1.04);"
 >
 	<img
-		id="flower1"
-		bind:this={flower1}
-		class="absolute w-[18vmin] h-auto opacity-0"
+		class="absolute w-3.5cm h-auto opacity-0 animate-[flower_240s_linear_infinite,blendIn_2s_forwards_var(--move)_0.5s,flowerTop_2s_forwards_var(--move)_0.5s]"
 		{src}
 		draggable="false"
 		{alt}
 		loading="eager"
 	/>
 	<img
-		id="flower2"
-		bind:this={flower2}
-		class="absolute w-[13vmin] h-auto opacity-0 -rotate-45"
+		class="absolute w-2.5cm h-auto opacity-0 -rotate-45 animate-[flower_110s_linear_infinite_reverse,blendIn_2s_forwards_var(--move)_0.5s,flowerLeft_2s_forwards_var(--move)_0.5s]"
 		{src}
 		draggable="false"
 		{alt}
 		loading="eager"
 	/>
 	<img
-		id="flower3"
-		bind:this={flower3}
-		class="absolute w-[11vmin] h-auto opacity-0"
+		class="absolute w-2cm h-auto opacity-0 animate-[flower_90s_linear_infinite,blendIn_2s_forwards_var(--move)_0.5s,flowerRight_2s_forwards_var(--move)_0.5s]"
 		{src}
 		draggable="false"
 		{alt}
 		loading="eager"
 	/>
 	<img
-		id="flower4"
-		bind:this={flower4}
-		class="absolute w-[5vmin] h-auto opacity-0 rotate-12"
+		class="absolute w-1cm h-auto opacity-0 rotate-12 animate-[flower_280s_linear_infinite_reverse,blendIn_2s_forwards_var(--move)_0.5s,flowerTopLeft_2s_forwards_var(--move)_0.5s]"
 		{src}
 		draggable="false"
 		{alt}
 		loading="eager"
 	/>
 	<img
-		id="flower5"
-		bind:this={flower5}
-		class="absolute w-[4vmin] h-auto opacity-0"
+		class="absolute w-0.9cm h-auto opacity-0 animate-[flower_280s_linear_infinite,blendIn_2s_forwards_var(--move)_0.5s,flowerTopRight_2s_forwards_var(--move)_0.5s]"
 		{src}
 		draggable="false"
 		{alt}
 		loading="eager"
 	/>
 	<div
-		class="text-15 absolute fw-bold code-font lg:text-20 xl:text-25 z-2 op-0
-  top-75% left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
-		bind:this={blendIn}
+		class="absolute fw-bold code-font text-25 z-2 op-0 top-75% left-1/2 -translate-x-1/2 -translate-y-1/2 text-center animate-[blendIn_2s_forwards_var(--move)_0.5s] z-99"
 	>
 		<ColorFadeText text="AYAKO" />
 	</div>
 </div>
 
 <style scoped>
-	#flower1 {
-		animation: flower1 240s linear infinite;
-	}
-
-	@keyframes flower1 {
+	@keyframes flower {
 		from {
 			transform: rotate(0deg);
 		}
@@ -233,31 +62,69 @@
 		}
 	}
 
-	#flower2 {
-		animation: flower2 110s linear infinite;
-	}
-
-	@keyframes flower2 {
+	@keyframes blendIn {
 		from {
-			transform: rotate(0deg);
+			opacity: 0;
 		}
-
 		to {
-			transform: rotate(-360deg);
+			opacity: 1;
 		}
 	}
 
-	#flower3 {
-		animation: flower3 90s linear infinite;
-	}
-
-	@keyframes flower3 {
+	@keyframes flowerTop {
 		from {
-			transform: rotate(0deg);
+			top: 3cm;
 		}
-
 		to {
-			transform: rotate(360deg);
+			top: 0;
+		}
+	}
+
+	@keyframes flowerLeft {
+		from {
+			left: calc(50% - 7vmin);
+			top: 3.5cm;
+			rotate: -45deg;
+		}
+		to {
+			left: calc(50% - 7cm);
+			top: 3.5cm;
+		}
+	}
+
+	@keyframes flowerRight {
+		from {
+			left: calc(50% - 3cm);
+			top: 3.5cm;
+			rotate: 12deg;
+		}
+		to {
+			left: calc(50% + 4.5cm);
+			top: 3.5cm;
+		}
+	}
+
+	@keyframes flowerTopLeft {
+		from {
+			left: calc(50% - 1.5cm);
+			top: 3.5cm;
+			rotate: -10deg;
+		}
+		to {
+			left: calc(50% - 4cm);
+			top: 2cm;
+		}
+	}
+
+	@keyframes flowerTopRight {
+		from {
+			left: calc(50% - 0.5cm);
+			top: 3.5cm;
+			rotate: 80deg;
+		}
+		to {
+			left: calc(50% + 3cm);
+			top: 2cm;
 		}
 	}
 </style>
