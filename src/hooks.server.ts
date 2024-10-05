d9e57d4a24c5f178ab9aa04326595e9d111ea590
Larsundso <Larsundso3@gmail.com>
2024-10-05 18:01:19 +0200

    remove ;

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -17,7 +17,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 		'max-age=31536000; includeSubDomains; preload;',
 	);
 	response.headers.append('X-Frame-Options', 'ALLOW-FROM https://top.gg;');
-	response.headers.append('X-Content-Type-Options', 'nosniff;');
+	response.headers.append('X-Content-Type-Options', 'nosniff');
 	response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin;');
 	response.headers.append('Permissions-Policy', 'camera=(), microphone=(), document-domain=();');
 
