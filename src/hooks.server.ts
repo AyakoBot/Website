005e6ed2b2581a61757cd5cba009260c847a0b34
Larsundso <Larsundso3@gmail.com>
2024-10-05 18:23:26 +0200

    add header

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,10 +8,10 @@ export const handle: Handle = async ({ event, resolve }) => {
 	});
 
 	response.headers.delete('x-sveltekit-page');
-	// response.headers.append(
-	// 	'Content-Security-Policy',
-	// 	"default-src 'self' cdn.ayakobot.com api.ayakobot.com; frame-ancestors 'self' https://top.gg; img-src 'self' cdn.ayakobot.com cdn.discordapp.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com; script-src-elem 'self' www.freeprivacypolicy.com; script-src 'self' www.freeprivacypolicy.com; connect-src 'self' api.ayakobot.com;",
-	// );
+	response.headers.append(
+		'Content-Security-Policy',
+		"frame-ancestors 'self' https://top.gg; default-src 'self'; script-src 'report-sample' 'self' https://static.cloudflareinsights.com/beacon.min.js https://static.hotjar.com/c/hotjar-2589609.js https://www.freeprivacypolicy.com/public/cookie-consent/4.1.0/cookie-consent.js https://www.googletagmanager.com/gtag/js; style-src 'report-sample' 'self' https://fonts.googleapis.com; object-src 'none'; base-uri 'self'; connect-src 'self'; font-src 'self' https://fonts.gstatic.com; frame-src 'self'; img-src 'self' https://cdn.ayakobot.com https://cdn.discordapp.com; manifest-src 'self'; media-src 'self'; report-uri https://6701672cc106bee79990a81f.endpoint.csper.io/?v=0; worker-src 'none';",
+	);
 	response.headers.append(
 		'Strict-Transport-Security',
 		'max-age=31536000; includeSubDomains; preload;',
